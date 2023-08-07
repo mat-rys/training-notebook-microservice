85cf22d6c38dd9f814be2655bc48f4076f04798a
Mateusz <matrys000@utp.edu.pl>
2023-08-07 14:39:38 +0200

    training-notebook Created angular project for front

---
+++ Front-end angular/notebook-angular/src/app/start-page/start-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.css']
+})
+export class StartPageComponent {
+  logoPath = 'https://i.pinimg.com/564x/18/15/1a/18151abd21b3a455500b5ba33b26a285.jpg';
+
+}
